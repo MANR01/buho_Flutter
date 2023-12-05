cb5519a1fdd275abcc3c38331e0a0617d812417b
MANR <micky01.mnnr@gmail.com>
2023-12-05 11:14:27 -0600

    Se añadieron las conexiones con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:buho/Pages/home/home.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() async {
-  WidgetsFlutterBinding();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  /*WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();*/
 
   runApp(const MyApp());
 }
