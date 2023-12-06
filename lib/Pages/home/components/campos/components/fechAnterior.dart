dfaac596a7914ab8010f4b320d476de0e2620e0a
MANR <micky01.mnnr@gmail.com>
2023-12-06 11:17:28 -0600

    Se añadio a los campos de texto el formato y mascara correspondiente

--- lib/Pages/home/components/campos/components/fechAnterior.dart
+++ lib/Pages/home/components/campos/components/fechAnterior.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class setFechAn extends StatelessWidget {
   final bool obscureText;
@@ -8,6 +9,8 @@ class setFechAn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var fechmask = MaskTextInputFormatter(
+        mask: '##/##/####', filter: {"#": RegExp(r'[0-9]')});
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(7),
@@ -15,11 +18,13 @@ class setFechAn extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       margin: const EdgeInsets.symmetric(horizontal: 20),
       child: TextFormField(
+        inputFormatters: [fechmask],
         keyboardType: TextInputType.datetime,
         textAlign: TextAlign.center,
         obscureText: obscureText,
         style: const TextStyle(fontSize: 14, color: Colors.black),
         decoration: const InputDecoration(
+            hintText: "DD/MM/AAAA",
             border: InputBorder.none,
             label: Text(
               'Fecha de Inicio',
